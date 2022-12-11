4bf38aee096ea1dd4bc3d4f632b5368d642f969f
Muhammad Tufail <115335714+Tufail46@users.noreply.github.com>
2022-12-11 11:38:23 +0500

    first commit

---
+++ bin/app.js
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+let operator = {
+    name: `opt`,
+    message: `What Operator you want to use?`,
+    type: "list",
+    choices: ["+", "-", "/", "*", "^"],
+};
+let first = {
+    name: `first_Number`,
+    message: `Write your First number`,
+    type: "number",
+};
+let second = {
+    name: `second_Number`,
+    message: `Write your Second number`,
+    type: "number",
+};
+let question = [first, operator, second];
+let restart = true;
+// To repeat the process of calculation untill you want to stop it
+do {
+    let answer = await inquirer.prompt(question);
+    const firstNumber = answer.first_Number;
+    const secondNumber = answer.second_Number;
+    const userOperator = answer.opt;
+    switch (userOperator) {
+        case "+":
+            console.log("Answer: ", firstNumber + secondNumber);
+            break;
+        case "-":
+            console.log("Answer: ", firstNumber - secondNumber);
+            break;
+        case "*":
+            console.log("Answer: ", firstNumber * secondNumber);
+            break;
+        case "/":
+            console.log("Answer: ", firstNumber / secondNumber);
+            break;
+        case "^":
+            console.log("Answer: ", firstNumber ** secondNumber);
+            break;
+    }
+    let continueUsing = await inquirer.prompt({
+        name: `askUser`,
+        message: `Do you want to continue => Y/N`,
+    });
+    restart =
+        continueUsing.askUser === "y" ||
+            continueUsing.askUser === "Y" ||
+            continueUsing.askUser === "yes" ||
+            continueUsing.askUser === "Yes" ||
+            continueUsing.askUser === "YES"
+            ? true
+            : false;
+} while (restart);
---
+++ root/app.ts
@@ -0,0 +1,72 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+type Operator = {
+  name: string;
+  message: string;
+  type: string;
+  choices: string[];
+};
+type First = {
+  name: string;
+  message: string;
+  type: string;
+};
+type Second = {
+  name: string;
+  message: string;
+  type: string;
+};
+let operator: Operator = {
+  name: `opt`,
+  message: `What Operator you want to use?`,
+  type: "list",
+  choices: ["+", "-", "/", "*", "^"],
+};
+let first: First = {
+  name: `first_Number`,
+  message: `Write your First number`,
+  type: "number",
+};
+let second: Second = {
+  name: `second_Number`,
+  message: `Write your Second number`,
+  type: "number",
+};
+let question = [first, operator, second];
+let restart = true;
+// To repeat the process of calculation untill you want to stop it
+do {
+  let answer = await inquirer.prompt(question);
+  const firstNumber = answer.first_Number;
+  const secondNumber = answer.second_Number;
+  const userOperator = answer.opt;
+  switch (userOperator) {
+    case "+":
+      console.log("Answer: ", firstNumber + secondNumber);
+      break;
+    case "-":
+      console.log("Answer: ", firstNumber - secondNumber);
+      break;
+    case "*":
+      console.log("Answer: ", firstNumber * secondNumber);
+      break;
+    case "/":
+      console.log("Answer: ", firstNumber / secondNumber);
+      break;
+    case "^":
+      console.log("Answer: ", firstNumber ** secondNumber);
+      break;
+  }
+  let continueUsing = await inquirer.prompt({
+    name: `askUser`,
+    message: `Do you want to continue => Y/N`,
+  });
+  restart =
+    continueUsing.askUser === "y" ||
+    continueUsing.askUser === "Y" ||
+    continueUsing.askUser === "yes" ||
+    continueUsing.askUser === "Yes" ||
+    continueUsing.askUser === "YES"
+      ? true
+      : false;
+} while (restart);
